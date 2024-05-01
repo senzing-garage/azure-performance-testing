7442e476bc4567b0e0c1154b7ecc7c0f72b914b7
roncewind <dad@lynntribe.net>
2024-05-01 07:22:10 -0700

    #5 added more loaders

--- perf.tf/producer_container_intances.tf
+++ perf.tf/producer_container_intances.tf
@@ -13,8 +13,8 @@ resource "azurerm_container_group" "sz_producer_0" {
   container {
     name   = "${random_pet.rg_name.id}-senzing-producer-0"
     image  = var.senzing_producer_image
-    cpu    = 0.5
-    memory = "1.0"
+    cpu    = 1
+    memory = "2.0"
     # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
 
     environment_variables = {
@@ -38,8 +38,8 @@ resource "azurerm_container_group" "sz_producer_0" {
   container {
     name   = "${random_pet.rg_name.id}-senzing-producer-1"
     image  = var.senzing_producer_image
-    cpu    = 0.5
-    memory = "1.0"
+    cpu    = 1
+    memory = "2.0"
     # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
 
     environment_variables = {
@@ -63,8 +63,8 @@ resource "azurerm_container_group" "sz_producer_0" {
   container {
     name   = "${random_pet.rg_name.id}-senzing-producer-2"
     image  = var.senzing_producer_image
-    cpu    = 0.5
-    memory = "1.0"
+    cpu    = 1
+    memory = "2.0"
     # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
 
     environment_variables = {
@@ -88,8 +88,120 @@ resource "azurerm_container_group" "sz_producer_0" {
   container {
     name   = "${random_pet.rg_name.id}-senzing-producer-3"
     image  = var.senzing_producer_image
-    cpu    = 0.5
-    memory = "1.0"
+    cpu    = 1
+    memory = "2.0"
+    # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
+
+    environment_variables = {
+      "AZURE_ANIMAL"                          = random_pet.rg_name.id
+      "LC_CTYPE"                              = "en_US.utf8"
+      "SENZING_AZURE_QUEUE_CONNECTION_STRING" = azurerm_servicebus_namespace.sz_service_bus.default_primary_connection_string
+      "SENZING_AZURE_QUEUE_NAME"              = azurerm_servicebus_queue.sz_queue.name
+      "SENZING_DEFAULT_DATA_SOURCE"           = "TEST"
+      "SENZING_DEFAULT_ENTITY_TYPE"           = "GENERIC"
+      "SENZING_INPUT_URL"                     = var.test_data_url
+      "SENZING_MONITORING_PERIOD_IN_SECONDS"  = "60"
+      "SENZING_READ_QUEUE_MAXSIZE"            = "200"
+      "SENZING_RECORD_MAX"                    = (var.number_of_records / 4) * 4
+      "SENZING_RECORD_MIN"                    = ((var.number_of_records / 4) * 3) + 1
+      "SENZING_RECORD_MONITOR"                = "100000"
+      "SENZING_RECORDS_PER_MESSAGE"           = "10"
+      "SENZING_SUBCOMMAND"                    = "gzipped-json-to-azure-queue"
+      "SENZING_THREADS_PER_PRINT"             = "30"
+    }
+  }
+}
+
+resource "azurerm_container_group" "sz_producer_1" {
+  name                = "${random_pet.rg_name.id}-continst"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_address_type     = "None"
+  os_type             = "Linux"
+  restart_policy      = "Never"
+  depends_on          = [azurerm_servicebus_queue.sz_queue]
+
+  # Senzing Producer, sends data to Azure Queue
+  container {
+    name   = "${random_pet.rg_name.id}-senzing-producer-0"
+    image  = var.senzing_producer_image
+    cpu    = 1
+    memory = "2.0"
+    # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
+
+    environment_variables = {
+      "AZURE_ANIMAL"                          = random_pet.rg_name.id
+      "LC_CTYPE"                              = "en_US.utf8"
+      "SENZING_AZURE_QUEUE_CONNECTION_STRING" = azurerm_servicebus_namespace.sz_service_bus.default_primary_connection_string
+      "SENZING_AZURE_QUEUE_NAME"              = azurerm_servicebus_queue.sz_queue.name
+      "SENZING_DEFAULT_DATA_SOURCE"           = "TEST"
+      "SENZING_DEFAULT_ENTITY_TYPE"           = "GENERIC"
+      "SENZING_INPUT_URL"                     = var.test_data_url
+      "SENZING_MONITORING_PERIOD_IN_SECONDS"  = "60"
+      "SENZING_READ_QUEUE_MAXSIZE"            = "200"
+      "SENZING_RECORD_MAX"                    = (var.number_of_records / 4) * 5
+      "SENZING_RECORD_MIN"                    = ((var.number_of_records / 4) * 4) + 1
+      "SENZING_RECORD_MONITOR"                = "100000"
+      "SENZING_RECORDS_PER_MESSAGE"           = "10"
+      "SENZING_SUBCOMMAND"                    = "gzipped-json-to-azure-queue"
+      "SENZING_THREADS_PER_PRINT"             = "30"
+    }
+  }
+  container {
+    name   = "${random_pet.rg_name.id}-senzing-producer-1"
+    image  = var.senzing_producer_image
+    cpu    = 1
+    memory = "2.0"
+    # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
+
+    environment_variables = {
+      "AZURE_ANIMAL"                          = random_pet.rg_name.id
+      "LC_CTYPE"                              = "en_US.utf8"
+      "SENZING_AZURE_QUEUE_CONNECTION_STRING" = azurerm_servicebus_namespace.sz_service_bus.default_primary_connection_string
+      "SENZING_AZURE_QUEUE_NAME"              = azurerm_servicebus_queue.sz_queue.name
+      "SENZING_DEFAULT_DATA_SOURCE"           = "TEST"
+      "SENZING_DEFAULT_ENTITY_TYPE"           = "GENERIC"
+      "SENZING_INPUT_URL"                     = var.test_data_url
+      "SENZING_MONITORING_PERIOD_IN_SECONDS"  = "60"
+      "SENZING_READ_QUEUE_MAXSIZE"            = "200"
+      "SENZING_RECORD_MAX"                    = (var.number_of_records / 4) * 6
+      "SENZING_RECORD_MIN"                    = ((var.number_of_records / 4) * 5) + 1
+      "SENZING_RECORD_MONITOR"                = "100000"
+      "SENZING_RECORDS_PER_MESSAGE"           = "10"
+      "SENZING_SUBCOMMAND"                    = "gzipped-json-to-azure-queue"
+      "SENZING_THREADS_PER_PRINT"             = "30"
+    }
+  }
+  container {
+    name   = "${random_pet.rg_name.id}-senzing-producer-2"
+    image  = var.senzing_producer_image
+    cpu    = 1
+    memory = "2.0"
+    # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
+
+    environment_variables = {
+      "AZURE_ANIMAL"                          = random_pet.rg_name.id
+      "LC_CTYPE"                              = "en_US.utf8"
+      "SENZING_AZURE_QUEUE_CONNECTION_STRING" = azurerm_servicebus_namespace.sz_service_bus.default_primary_connection_string
+      "SENZING_AZURE_QUEUE_NAME"              = azurerm_servicebus_queue.sz_queue.name
+      "SENZING_DEFAULT_DATA_SOURCE"           = "TEST"
+      "SENZING_DEFAULT_ENTITY_TYPE"           = "GENERIC"
+      "SENZING_INPUT_URL"                     = var.test_data_url
+      "SENZING_MONITORING_PERIOD_IN_SECONDS"  = "60"
+      "SENZING_READ_QUEUE_MAXSIZE"            = "200"
+      "SENZING_RECORD_MAX"                    = (var.number_of_records / 4) * 7
+      "SENZING_RECORD_MIN"                    = ((var.number_of_records / 4) * 6) + 1
+      "SENZING_RECORD_MONITOR"                = "100000"
+      "SENZING_RECORDS_PER_MESSAGE"           = "10"
+      "SENZING_SUBCOMMAND"                    = "gzipped-json-to-azure-queue"
+      "SENZING_THREADS_PER_PRINT"             = "30"
+    }
+  }
+  container {
+    name   = "${random_pet.rg_name.id}-senzing-producer-3"
+    image  = var.senzing_producer_image
+    cpu    = 1
+    memory = "2.0"
     # command = ["/bin/bash", "-c", "while true; do echo grumble $(date); sleep 600;done"]
 
     environment_variables = {
@@ -103,7 +215,7 @@ resource "azurerm_container_group" "sz_producer_0" {
       "SENZING_MONITORING_PERIOD_IN_SECONDS"  = "60"
       "SENZING_READ_QUEUE_MAXSIZE"            = "200"
       "SENZING_RECORD_MAX"                    = var.number_of_records
-      "SENZING_RECORD_MIN"                    = ((var.number_of_records / 4) * 3) + 1
+      "SENZING_RECORD_MIN"                    = ((var.number_of_records / 4) * 7) + 1
       "SENZING_RECORD_MONITOR"                = "100000"
       "SENZING_RECORDS_PER_MESSAGE"           = "10"
       "SENZING_SUBCOMMAND"                    = "gzipped-json-to-azure-queue"
